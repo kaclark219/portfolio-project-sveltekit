2569499e1c95d60f52604e6661db537b7a44ebfa
Katelyn Clark <kaclark219@gmail.com>
2024-10-14 19:08:10 -0500

    env setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import '../style/style.css';
+</script>
+<nav>
+    <ul>
+        <li>
+            <a href="./">Home</a>
+        </li>
+        <li>
+            <a href="./about-me">About Me</a>
+        </li>
+        <li>
+            <a href="./projects">Projects</a>
+        </li>
+    </ul>
+</nav>
+<h1>Home</h1>
+<p>This is the home page.</p>
---
+++ src/routes/about-me/+page.svelte
@@ -0,0 +1,2 @@
+<h1>About Me</h1>
+<p>This is an about page.</p>
