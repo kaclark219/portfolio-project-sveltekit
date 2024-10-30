89dcdc0df463e6a4c3c3b7cb9cfb096970d1db35
Katelyn Clark <kaclark219@gmail.com>
2024-10-30 14:24:49 -0500

    intro section added.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,3 +18,23 @@
         </ul>
     </nav>
 </div>
+<div class="body">
+    <div class="intro">
+        <div class="intro-left">
+            <h1 class="title">Software Developer & Game Designer</h1>
+            <p class="intro-paragraph">Hi! I'm Katelyn, a senior computer science student at Texas A&M with a concentration in HCI/d for games + the web, & a minor in graphic design. Currently, I'm applying to graduate programs in computer science & am looking for summer internships in the game industry! My research interest is in the field of Human-Computer Design & Visualization. I enjoy full-stack software development, game design theory, and digital illustration. It's nice to meet you!</p>
+        </div>
+        <div class="intro-right">
+            <img src="" alt="Headshot">
+        </div>
+    </div>
+    <div class="skills">
+
+    </div>
+    <div class="project-outline">
+
+    </div>
+    <div class="footer">
+
+    </div>
+</div>
